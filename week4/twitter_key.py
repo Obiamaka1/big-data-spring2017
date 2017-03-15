3e1b614ca6039ddd183394780966698c403da97e
Obiamaka Ude <obiamakaude@gmail.com>
2017-03-15 14:51:45 -0400

    homework changes
    obiamaka’s jupiter changes

--- week4/twitter_key.py
+++ week4/twitter_key.py
@@ -1,2 +1,2 @@
-api_key = "put your api key here between the quotes"
-api_secret = "put your secret key here between the quotes"
+api_key = "CBpgdBn9bvJF1B61HI2fTdnAG"
+api_secret = "ZcyuPn7F9u11RN77984vwcJCbDzfTgyvE3El3daJwK141f7neN"
